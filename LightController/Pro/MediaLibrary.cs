7d31292398e3ef433e7e1b09e0b4795c9405b587
avaness <austinvaness@gmail.com>
2022-08-01 22:34:44 -0500

    Remove OpenCv

--- LightController/Pro/MediaLibrary.cs
+++ LightController/Pro/MediaLibrary.cs
@@ -4,7 +4,6 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Runtime.InteropServices;
-using OpenCvSharp;
 using Colourful;
 
 namespace LightController.Pro
@@ -108,52 +107,6 @@ namespace LightController.Pro
                 resultPixels[i] = new ColorRGB(red, green, blue);
             }
             return resultPixels;
-        }
-
-        public static void DrawHistogram(string image)
-        {
-            Mat src = Cv2.ImRead(image, ImreadModes.Color);
-            src = src.CvtColor(ColorConversionCodes.BGR2HSV_FULL);
-
-            // Histogram view
-            const int Width = 260, Height = 200;
-            Mat render = new Mat(new OpenCvSharp.Size(Width, Height), MatType.CV_8UC3, Scalar.All(255));
-
-            // Calculate histogram
-            Mat hist = new Mat();
-            int[] hdims = { 256 }; // Histogram size for each dimension
-            Rangef[] ranges = { new Rangef(0, 256), }; // min/max 
-            Cv2.CalcHist(
-                new Mat[] { src },
-                new int[] { 0 },
-                null,
-                hist,
-                1,
-                hdims,
-                ranges);
-
-            // Get the max value of histogram
-            double minVal, maxVal;
-            Cv2.MinMaxLoc(hist, out minVal, out maxVal);
-
-            Scalar color = Scalar.All(100);
-            // Scales and draws histogram
-            hist = hist * (maxVal != 0 ? Height / maxVal : 0.0);
-            for (int j = 0; j < hdims[0]; ++j)
-            {
-                int binW = (int)((double)Width / hdims[0]);
-                render.Rectangle(
-                    new OpenCvSharp.Point(j * binW, render.Rows - (int)(hist.Get<float>(j))),
-                    new OpenCvSharp.Point((j + 1) * binW, render.Rows),
-                    color,
-                    -1);
-            }
-
-            using (new Window("Image", src, WindowFlags.AutoSize | WindowFlags.FreeRatio))
-            using (new Window("Histogram", render, WindowFlags.AutoSize | WindowFlags.FreeRatio))
-            {
-                Cv2.WaitKey();
-            }
         }
     }
 }
