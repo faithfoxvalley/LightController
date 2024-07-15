428f8f146ce7fb62e9ddecdce4d6c632796bee44
avaness <austinvaness@gmail.com>
2024-07-15 15:49:14 -0500

    Better config defaults

--- LightController/Config/ProPresenterConfig.cs
+++ LightController/Config/ProPresenterConfig.cs
@@ -1,16 +1,30 @@
-﻿using YamlDotNet.Serialization;
+﻿using System;
+using System.IO;
+using YamlDotNet.Serialization;
 
 namespace LightController.Config
 {
     public class ProPresenterConfig
     {
         [YamlMember(Description = "IP and port from ProPresenter network settings in this form: http://ip-address:port/v1/")]
-        public string ApiUrl { get; set; }
-        
+        public string ApiUrl { get; set; } = "http://localhost:1025/v1/";
+
         [YamlMember(Description = "Path to the media assets folder")]
         public string MediaAssetsPath { get; set; }
 
         [YamlMember(Description = "Number of media processors to use at the same time")]
         public int MaxMediaProcessors { get; set; } = 2;
+
+        public ProPresenterConfig()
+        {
+            try
+            {
+                MediaAssetsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "ProPresenter", "Media", "Assets");
+            }
+            catch (Exception)
+            {
+                MediaAssetsPath = null;
+            }
+        }
     }
 }
